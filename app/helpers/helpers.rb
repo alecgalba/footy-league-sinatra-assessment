a0d7da327583fcd7566acc10c44d8c44abc7ac17
Alec Alba <alecgalba@gmail.com>
2017-09-12 13:59:55 +0000

    built first draft of helper methods

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,3 +1,21 @@
 module Helpers
-  
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def redirect_if_not_logged_in
+    if !logged_in?
+      redirect '/login'
+    end
+  end
+
+  def current_user
+    if !@user
+      @user = User.find(session[:user_id])
+    else
+      @user
+    end
+  end
+
 end
