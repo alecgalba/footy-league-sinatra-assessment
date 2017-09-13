ba4744adeaef2f949704a00b585bf163328d9ad4
Alec Alba <alecgalba@gmail.com>
2017-09-13 21:41:26 +0000

    first of many deletes. Deleted database

--- db/migrate/20170911211532_create_leagues.rb
+++
@@ -1,9 +0,0 @@
-class CreateLeagues < ActiveRecord::Migration[5.1]
-  def change
-    create_table :leagues do |t|
-      t.integer :league_id
-      t.integer :team_id
-      t.timestamps null: false
-    end
-  end
-end
