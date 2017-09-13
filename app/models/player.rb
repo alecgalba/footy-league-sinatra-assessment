9d1d61e989c33c1bdcbc9155310a80bd8c494049
Alec Alba <alecgalba@gmail.com>
2017-09-13 02:08:52 +0000

    slightly modified slug method to reflect correct arguments

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,7 +5,7 @@ class Player < ActiveRecord::Base
   has_many :leagues, through: :teams
 
   def slug
-    self.username.downcase.gsub(" ", "-")
+    self.email.downcase.gsub(" ", "-")
   end
 
   def self.find_by_slug(slug)
