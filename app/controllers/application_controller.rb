c15dd95d92b38fce07e07bac885ec32b67138efd
Alec Alba <alecgalba@gmail.com>
2017-09-13 22:14:46 +0000

    built app controller with homepage erb

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "unlockable"
+  end
+
+  get '/' do
+    erb :index
+  end
+end
