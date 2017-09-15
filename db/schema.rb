a8ebfded5d6ef9c3eaceb549f3334b4bf95ff636
Alec Alba <alecgalba@gmail.com>
2017-09-15 20:03:42 +0000

    tried making it so you can ad players but stopped

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913220408) do
+ActiveRecord::Schema.define(version: 20170915200104) do
 
   create_table "league_teams", force: :cascade do |t|
     t.integer "team_id"
