1bc523aa80c7c3a07fe601bb546319b3bf9e17c7
Alec Alba <alecgalba@gmail.com>
2017-09-18 16:12:00 +0000

    removed slug methods because they were unnecessary to function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,6 @@ class UsersController < ApplicationController
     end
   end
 
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    erb :"/users/show"
-  end
-
   post '/signup' do
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
       redirect to '/signup'
