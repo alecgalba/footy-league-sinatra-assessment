e19fd7790db1b729dc6088c7e122f65cd5460f95
Alec Alba <alecgalba@gmail.com>
2017-09-18 16:12:34 +0000

    removed unnessary route that wasnt being used

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,4 @@ class User < ActiveRecord::Base
   has_many :teams
   has_secure_password
 
-  def slug
-    self.username.downcase.gsub(" ", "-")
-  end
-
-  def self.find_by_slug(slug)
-    self.all.find{|a| a.slug == slug}
-  end
 end
